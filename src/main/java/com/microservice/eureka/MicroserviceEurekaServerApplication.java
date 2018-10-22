bf141a2e15eaaa145fde53dd903e8579882804e1
ROBIN SINGH <robinsingh0210@gmail.com>
2018-10-22 15:34:59 +0530

    Implemented the Spring cloud using Zuul Gateway.

--- src/main/java/com/microservice/eureka/MicroserviceEurekaServerApplication.java
+++ src/main/java/com/microservice/eureka/MicroserviceEurekaServerApplication.java
@@ -3,9 +3,11 @@ package com.microservice.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableZuulProxy
 public class MicroserviceEurekaServerApplication {
 
 	public static void main(String[] args) {
